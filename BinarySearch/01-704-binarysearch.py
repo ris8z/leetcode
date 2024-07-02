64108214a084f3d966e0c414356d9baf64c3d25c
esposig2 <giuseppe.esposito2@mail.dcu.ie>
2024-07-02 01:23:30 +0100

    /

--- BinarySearch/01-704-binarysearch.py
+++ BinarySearch/01-704-binarysearch.py
@@ -30,7 +30,37 @@ class Solution:
             return -1
         return fun1(nums, target)
         
+        def fun1_1(a, q):
+            #per riprendere il discorso del fatto che non finisci in un loop infinito
+            #se inverti il ragionamento a roundi sempre up
 
+            #in questo modo se hai un array tipo [1, 2, 2, 2, 3] target 2
+            #il pointer si fermera sull'ultimo 2, result = 4
+
+            left, right = 0, len(a) - 1
+            while left < right:
+                mid = math.ceil(float(left + right) / 2)
+                
+                if a[mid] > q:
+                    right = mid - 1
+                else:
+                    left = mid
+            #then the same as fun1_1 
+
+        #IMPORTANTE
+        #se non ci sono i target, il risulato
+        #(di) fun1    ti puntera al'elemento subito piu' grande  di target
+        
+
+        #(di) fun1_1  ti puntera al'elemento subito piu' piccolo di target
+        #(utile per trovami il minore o uguale di questo Timestap ex)
+
+        #fun1   arrotonda a left  (bisect left ) e ti da il left  most number
+        #fun1_1 arrotonda a right (bisect right) e ti da il right most number
+        #non fa na piega
+        
+        
+        #CLASSICA
         #Utile quando abbiamo un target prescisio che possiamo individure
         #as soon as we hit the q we return it
         #we can easly check if our guess is equal to the query
